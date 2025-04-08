a284bf015db59dc438190c5180dbbc719dc99407
Mzg. <jchallaksantos@gmail.com>
2025-04-08 16:00:16 -0300

    init

---
+++ src/main/java/com/jchallak/BancoAzimov/BancoAzimovApplication.java
@@ -0,0 +1,13 @@
+package com.jchallak.BancoAzimov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoAzimovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoAzimovApplication.class, args);
+	}
+
+}
